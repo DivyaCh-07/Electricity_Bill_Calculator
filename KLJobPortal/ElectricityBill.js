0dd7f2e2d9350e0a0ad2120bc428fadef5a7c489
DivyaCh-07 <2500080151@kluniversity.in>
2025-11-29 21:02:30 +0530

    Add files via upload

---
+++ KLJobPortal/ElectricityBill.js
@@ -0,0 +1,17 @@
+function CalculateBill() {
+    let units = document.getElementById("units").value;
+    let bill = 0;
+
+    if (units <= 100) {
+        bill = units * 5;
+    }
+    else if (units <= 200) {
+        bill = (100 * 5) + (units - 100) * 7;
+    }
+    else {
+        bill = (100 * 5) + (100 * 7) + (units - 200) * 10;
+    }
+
+    document.getElementById("result").innerHTML = 
+        "Total Electricity Bill: ₹ " + bill.toFixed(2);
+}
